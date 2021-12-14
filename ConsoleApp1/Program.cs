011bdaec84cf1f7d54b1ef07cb3cbe6b46bdbba2
P4lerm0 <sasaermakov77@gmail.com>
2021-12-14 12:31:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Melyakina.Lab5.Num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+            int[,] a = new int[n, n];
+            int dmax = a[0, 0];
+            int di = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("Введите {0}-ю строку", i + 1);
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i, i] > dmax)
+                {
+                    dmax = a[i, i];
+                    di = i;
+                }
+
+            }
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(a[di, j]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab6.part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите {0}-й элемент", i + 1);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] % 2 == 1 | a[i] < 0)
+                    sum = sum + a[i];
+            }
+            Console.Write(sum);
+
+        }
+    }
+}
